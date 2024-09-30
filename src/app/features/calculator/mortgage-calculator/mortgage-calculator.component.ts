33481e7d2a51826005ae02bee752ea68be2bf9b6
Catalina Popa <catalina.popa@fundmore.ai>
2024-09-30 17:21:26 +0300

    add results style and icons

--- src/app/features/calculator/mortgage-calculator/mortgage-calculator.component.ts
+++ src/app/features/calculator/mortgage-calculator/mortgage-calculator.component.ts
@@ -10,7 +10,7 @@ export class MortgageCalculatorComponent {
   @Input() monthlyPayment: number = 0;
   @Input() debtToIncome: number = 0;
   @Input() loanToValue: number = 0;
-  // add term
+  @Input() term: number = 0;
 
   infoText: string =
     'Here’s our estimate of your monthly mortgage repayments. In general, the maximum amount you can borrow is 4.5x your joint income and you need to have at least 10% of the value of the property saved as a deposit for lenders to accept your application.';
